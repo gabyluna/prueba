7de0c61f67be5a0e6df0b03464ebb60c96591875
administrador <administrador@localhost>
2017-10-24 13:11:33 +0100

    test endpoint

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,30 @@
 package com.example.demo;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class DemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello/pedrito").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
 
 }
